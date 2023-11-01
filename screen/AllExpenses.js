e75b1671ec1939cbe14cd0fcc0e9b405f909ccc2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-01 15:15:11 +0100

    chore: manage expenses

--- screen/AllExpenses.js
+++ screen/AllExpenses.js
@@ -1,7 +1,7 @@
 import { Text } from "react-native";
 
 function AllExpenses() {
-  return <Text> This is all expense page </Text>;
+  return <Text> This is all expense screen </Text>;
 }
 
 export default AllExpenses;
