195f94303998f88b6c598c2b60d5ef26c750c86e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-01 17:01:11 +0100

    chore: Express output

--- components/Expenses/ExpenseOutput.js
+++ components/Expenses/ExpenseOutput.js
@@ -2,10 +2,10 @@ import { View } from "react-native";
 import ExpenseSummary from "./ExpenseSummary";
 import ExpenseList from "./ExpenseList";
 
-function ExpenseOutput() {
+function ExpenseOutput({ expence, periodName }) {
   return (
     <View>
-      <ExpenseSummary />
+      <ExpenseSummary expence={expence} periodName={periodName} />
       <ExpenseList />
     </View>
   );
