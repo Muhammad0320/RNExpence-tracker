7b22bc21b2b06dc027de0e9029329ca5cc0a0012
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-01 16:50:50 +0100

    feat: add compoent and dummy data for Expense

---
+++ components/Expenses/ExpenseSummary.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+function ExpenseSummary() {
+  return (
+    <View>
+      <Text> Last 7 days </Text>
+      <Text> $199.99 </Text>
+    </View>
+  );
+}
+
+export default ExpenseSummary;
