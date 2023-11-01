c242b793b2443bdca8bf1a56315532beeeda088c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-01 15:40:49 +0100

    feat: add the Tabs navigation

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "react-native";
 import ManageExpense from "./screen/ManageExpense";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AllExpenses from "./screen/AllExpenses";
+import RecentExpenses from "./screen/RecentExpenses";
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-  // const ExpenseOverview = () => (
+const Tabs = createBottomTabNavigator();
 
-  // )
+export default function App() {
+  const ExpenseOverview = () => (
+    <Tabs.Navigator>
+      <Tabs.Screen name="AllExpense" component={AllExpenses} />
+      <Tabs.Screen name="RecentExpence" component={RecentExpenses} />
+    </Tabs.Navigator>
+  );
 
   return (
     <>
@@ -16,6 +24,7 @@ export default function App() {
 
       <NavigationContainer>
         <Stack.Navigator>
+          <Stack.Screen name="ExoenseOverview" component={ExpenseOverview} />
           <Stack.Screen name="ManageExpense" component={ManageExpense} />
         </Stack.Navigator>
       </NavigationContainer>
