a14e06a66b310e6112a7192fdf513869469d962a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-02 04:45:55 +0100

    chore: add styled to expense summary

--- components/Expenses/ExpenseSummary.js
+++ components/Expenses/ExpenseSummary.js
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function ExpenseSummary({ expense, periodName }) {
   const totalExpencePrice = expense.reduce((acc, sum) => {
@@ -6,11 +7,35 @@ function ExpenseSummary({ expense, periodName }) {
   }, 0);
 
   return (
-    <View>
-      <Text> {periodName} </Text>
-      <Text> ${totalExpencePrice.toFixed(2)} </Text>
+    <View style={styles.container}>
+      <Text style={styles.period}> {periodName} </Text>
+      <Text style={styles.sum}> ${totalExpencePrice.toFixed(2)} </Text>
     </View>
   );
 }
 
 export default ExpenseSummary;
+
+const styles = StyleSheet.create({
+  container: {
+    overflow: "hidden",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 6,
+    padding: 6,
+    margin: 8,
+  },
+
+  period: {
+    color: GlobalStyles.colors.primary400,
+    fontSize: 12,
+  },
+
+  sum: {
+    color: GlobalStyles.colors.primary500,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
