156dc9be9cfc6394d69c7e41a7a55c65fe3aa3c3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-02 13:04:06 +0100

    chore: add expense item style

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,7 +1,66 @@
-import { Pressable } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
-function ExpenseItem() {
-  return <Pressable> </Pressable>;
+function ExpenseItem({ amount, date, description }) {
+  return (
+    <Pressable>
+      <View style={style.contaner}>
+        <View style={style.descriptionContainer}>
+          <Text style={style.descText}> {description} </Text>
+          <Text style={style.descText}> {date} </Text>
+        </View>
+
+        <View style={style.amountContainer}>
+          <Text styl={style.amountText}> {amount} </Text>
+        </View>
+      </View>
+    </Pressable>
+  );
 }
 
 export default ExpenseItem;
+
+const style = StyleSheet.create({
+  contaner: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: GlobalStyles.colors.primary500,
+    borderRadius: 8,
+    margin: 8,
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    elevation: 4,
+    shadowColor: GlobalStyles.colors.gray500,
+    shadowOffset: { width: 2, height: 1 },
+    shadowRadius: 3,
+    shadowOpacity: 0.4,
+  },
+
+  descriptionContainer: {
+    rowGap: 6,
+  },
+
+  descText: {
+    color: GlobalStyles.colors.primary50,
+  },
+
+  descriptionText: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+
+  amountText: {
+    color: GlobalStyles.colors.primary700,
+    fontWeight: "bold",
+  },
+
+  amountContainer: {
+    backgroundColor: "#fff",
+    marginVertical: 8,
+    paddingVertical: 6,
+    paddingHorizontal: 4,
+    borderRadius: 6,
+    minWidth: 80,
+  },
+});
