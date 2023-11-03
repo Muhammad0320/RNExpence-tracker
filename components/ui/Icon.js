79d88166ceb6e8e8e8280e68c3089397f5c954f5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-03 06:50:49 +0100

    chore: complete icon.js file

--- components/ui/Icon.js
+++ components/ui/Icon.js
@@ -1,5 +1,30 @@
-function Icon() {
-  return <div></div>;
+import { Pressable, StyleSheet, View } from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+
+function Icon({ color, size, name, onPress }) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
+      <View style={styles.iconContainer}>
+        <Ionicons name={name} color={color} size={size} />
+      </View>
+    </Pressable>
+  );
 }
 
 export default Icon;
+
+const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
+
+  iconContainer: {
+    padding: 6,
+    marginHorizontal: 8,
+    marginVertical: 2,
+  },
+});
