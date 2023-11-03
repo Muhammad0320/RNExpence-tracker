6df65f36c76f846f9bdb0b73c14b0ce600a581da
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-03 09:04:16 +0100

    chore: add delete button condotionally

--- screen/ManageExpense.js
+++ screen/ManageExpense.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { View } from "react-native";
+import { GlobalStyles } from "../constants/styles";
+import Icon from "../components/ui/Icon";
 
 function ManageExpense({ navigation, route }) {
   const editedId = route.params?.id;
@@ -10,7 +13,31 @@ function ManageExpense({ navigation, route }) {
     });
   }, [navigation, editedId]);
 
-  return <Text> ManageExpense screen </Text>;
+  return (
+    <View style={style.container}>
+      {editedId && (
+        <View style={style.deleteContainer}>
+          <Icon color={GlobalStyles.colors.error500} size={36} name="trash" />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: GlobalStyles.colors.primary800,
+    padding: 24,
+  },
+
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopColor: GlobalStyles.colors.primary200,
+    borderTopWidth: 2,
+    alignItems: "center",
+  },
+});
