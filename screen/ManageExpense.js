c965e97c7acb8108a11a0eddf7b3029937ec40cc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-03 07:55:41 +0100

    chore: programatically set the manage expense header

--- screen/ManageExpense.js
+++ screen/ManageExpense.js
@@ -1,6 +1,17 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ navigation, route }) {
+  const editedId = route.params?.id;
+
+  const isEditing = !!editedId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return <Text> ManageExpense screen </Text>;
 }
 
