9099a3310a7ae6c60fb6927d8bf3fbd332c3d961
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-04 06:18:35 +0100

    chore: add a reusable function to foormat date

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,3 +1,7 @@
 export const dateFormatter = (date) => {
   return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} `;
 };
+
+export const calcDatePast = (date, day) => {
+  return new Date(date.getFullYear(), date.getMonth, date.getDate - day);
+};
