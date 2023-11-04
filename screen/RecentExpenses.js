a5bb5eafbfeb10b2a82567c81e29c2d10fa9caf3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-04 06:21:57 +0100

    chore: add recent expense functionalty

--- screen/RecentExpenses.js
+++ screen/RecentExpenses.js
@@ -1,7 +1,17 @@
 import ExpenseOutput from "../components/Expenses/ExpenseOutput";
+import { useExpenseContext } from "../store/expensesContext";
+import { calcDatePast } from "../utils/dateFormat";
 
 function RecentExpenses() {
-  return <ExpenseOutput periodName="Last 7 days" />;
+  const { expenses } = useExpenseContext();
+
+  const last7daysDate = calcDatePast(new Date(), 7);
+
+  const recentExpense = expenses.filter(
+    (expense) => expense.date >= last7daysDate
+  );
+
+  return <ExpenseOutput expense={recentExpense} periodName="Last 7 days" />;
 }
 
 export default RecentExpenses;
