bb65e93c6fcbb70503a86a3593e1eba6a1fa63ff
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-04 06:46:58 +0100

    chore: rendered the fallback text conditionally

--- components/Expenses/ExpenseOutput.js
+++ components/Expenses/ExpenseOutput.js
@@ -1,13 +1,19 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import ExpenseSummary from "./ExpenseSummary";
 import ExpenseList from "./ExpenseList";
 import { GlobalStyles } from "../../constants/styles";
 
-function ExpenseOutput({ expense, periodName }) {
+function ExpenseOutput({ expense, periodName, fallback }) {
+  let output = <Text style={styles.textStyle}> {fallback} </Text>;
+
+  if (expense.length) {
+    output = <ExpenseList expense={expense} />;
+  }
+
   return (
     <View style={styles.container}>
       <ExpenseSummary expense={expense} periodName={periodName} />
-      <ExpenseList expense={expense} />
+      {output}
     </View>
   );
 }
