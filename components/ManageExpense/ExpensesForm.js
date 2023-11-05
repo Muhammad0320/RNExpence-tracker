dcc1ca5a842578bef67a4e8b6601534063bc1659
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-05 07:34:07 +0100

    chore: add expense form

--- components/ManageExpense/ExpensesForm.js
+++ components/ManageExpense/ExpensesForm.js
@@ -1,8 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import Inputs from "./Inputs";
 import { useState } from "react";
 
-function ExpensesForm() {
+function ExpensesForm({ onConfirm, onCancel, buttonText }) {
   const [inputValues, setInputValues] = useState({
     amount: "",
     date: "",
@@ -18,43 +18,57 @@ function ExpensesForm() {
     });
   };
 
-  return (
-    <View style={styles.form}>
-      <Text style={styles.title}> Add Expense </Text>
-      <View style={styles.formRow}>
-        <Inputs
-          label="Amount"
-          style={styles.inputStyle}
-          inputConfig={{
-            KeyboardType: "decimal-pad",
+  const handleConfirm = () => {};
 
-            onChangeText: () => handleTextInput("amount"),
-            value: inputValues.amount,
-          }}
-        />
+  return (
+    <>
+      <View style={styles.form}>
+        <Text style={styles.title}> Add Expense </Text>
+        <View style={styles.formRow}>
+          <Inputs
+            label="Amount"
+            style={styles.inputStyle}
+            inputConfig={{
+              KeyboardType: "decimal-pad",
+
+              onChangeText: () => handleTextInput("amount"),
+              value: inputValues.amount,
+            }}
+          />
+          <Inputs
+            label="Date"
+            style={styles.inputStyle}
+            inputConfig={{
+              placeholder: "YYYY-MM-DD",
+              maxLength: 10,
+              onChangeText: () => handleTextInput("date"),
+              value: inputValues.date,
+            }}
+          />
+        </View>
+
         <Inputs
-          label="Date"
-          style={styles.inputStyle}
+          label="description"
           inputConfig={{
-            placeholder: "YYYY-MM-DD",
-            maxLength: 10,
-            onChangeText: () => handleTextInput("date"),
-            value: inputValues.date,
+            multiline: true,
+            autoCorrect: true,
+            autoCapitalize: "sentences",
+            onChangeText: () => handleTextInput("description"),
+            value: inputValues.description,
           }}
         />
       </View>
 
-      <Inputs
-        label="description"
-        inputConfig={{
-          multiline: true,
-          autoCorrect: true,
-          autoCapitalize: "sentences",
-          onChangeText: () => handleTextInput("description"),
-          value: inputValues.description,
-        }}
-      />
-    </View>
+      <View style={styles.buttonContainer}>
+        <Button style={styles.buttonStyle} mode="flat" onPress={onCancel}>
+          {" "}
+          cancel{" "}
+        </Button>
+        <Button style={styles.buttonStyle} onPress={handleConfirm}>
+          {buttonText}
+        </Button>
+      </View>
+    </>
   );
 }
 
@@ -81,5 +95,15 @@ const styles = StyleSheet.create({
 
   inputStyle: {
     flex: 1,
+  },
+
+  buttonContainer: {
+    flexDirection: "row",
+    columnGap: 20,
+    alignContent: "center",
+  },
+
+  buttonStyle: {
+    minWidth: 150,
   },
 });
