660ba15eff6b60ab0db1cee4bc449a34d670e90a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-05 05:32:14 +0100

    chore: addd configuration text to expense form

--- components/ManageExpense/ExpensesForm.js
+++ components/ManageExpense/ExpensesForm.js
@@ -1,7 +1,24 @@
 import { View } from "react-native";
+import Inputs from "./Inputs";
 
 function ExpensesForm() {
-  return <View></View>;
+  return (
+    <View>
+      <Inputs
+        label="Amount"
+        inputConfig={{ KeyboardType: "decimal-pad", onChangeText: () => {} }}
+      />
+      <Inputs
+        label="Date"
+        inputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Inputs />
+    </View>
+  );
 }
 
 export default ExpensesForm;
