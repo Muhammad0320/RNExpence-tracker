7f4f85861d8cb69e769c57f9cc78efefb8296200
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-05 06:21:22 +0100

    chore: style the input element s conditionally

--- components/ManageExpense/Inputs.js
+++ components/ManageExpense/Inputs.js
@@ -1,12 +1,43 @@
-import { Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function Inputs({ inputConfig, label }) {
+  const inputStyle = [styles.inputStyle];
+
+  if (inputConfig.multiline) {
+    inputStyle.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text> {label} </Text>
-      <TextInput {...inputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}> {label} </Text>
+      <TextInput {...inputConfig} style={inputStyle} />
     </View>
   );
 }
 
 export default Inputs;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 6,
+    marginVertical: 10,
+    rowGap: 5,
+  },
+
+  label: {
+    fontSize: 14,
+    color: GlobalStyles.colors.primary100,
+  },
+
+  inputStyle: {
+    fontSize: 18,
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 7,
+  },
+
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
