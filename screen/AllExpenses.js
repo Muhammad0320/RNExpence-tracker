b0a93d3f88bc18f95f11012f81c56e5595f68b4a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-08 04:55:44 +0100

    chore: add a state login for fetchig state

--- screen/AllExpenses.js
+++ screen/AllExpenses.js
@@ -1,14 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ExpenseOutput from "../components/Expenses/ExpenseOutput";
 import { useExpenseContext } from "../store/expensesContext";
 import { getExpensesApi } from "../utils/http";
 
 function AllExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
+
   const { expenses, fetchExpenseData } = useExpenseContext();
 
   useEffect(() => {
     const fetchFunc = async () => {
+      setIsFetching(true);
       const expenses = await getExpensesApi();
+      setIsFetching(false);
 
       fetchExpenseData(expenses);
     };
