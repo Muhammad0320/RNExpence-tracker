c96f19d6868d56058526e4f3fa7c6c529d90ee29
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-08 06:35:29 +0100

    chore: add error overlay logic and component to the get expense

--- screen/AllExpenses.js
+++ screen/AllExpenses.js
@@ -3,23 +3,34 @@ import ExpenseOutput from "../components/Expenses/ExpenseOutput";
 import { useExpenseContext } from "../store/expensesContext";
 import { getExpensesApi } from "../utils/http";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import ErrorOverlay from "../components/ui/ErrorOverlay";
 
 function AllExpenses() {
   const [isFetching, setIsFetching] = useState(true);
+
+  const [error, setError] = useState(null);
 
   const { expenses, fetchExpenseData } = useExpenseContext();
 
   useEffect(() => {
     const fetchFunc = async () => {
-      setIsFetching(true);
-      const expenses = await getExpensesApi();
-      setIsFetching(false);
+      try {
+        setIsFetching(true);
+        const expenses = await getExpensesApi();
+        fetchExpenseData(expenses);
+      } catch (error) {
+        setError("Could not fetch Expense");
+      }
 
-      fetchExpenseData(expenses);
+      setIsFetching(false);
     };
 
     fetchFunc();
   }, []);
+
+  if (!isFetching && error) {
+    return <ErrorOverlay message={error} />;
+  }
 
   if (isFetching) {
     return <LoadingOverlay />;
