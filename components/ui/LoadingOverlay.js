c3ef7f70c61831d244540ff03a83ad949a89a76d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-08 05:00:41 +0100

    chore: complete the loading overlay comp

--- components/ui/LoadingOverlay.js
+++ components/ui/LoadingOverlay.js
@@ -1,7 +1,22 @@
-import { View } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 function LoadingOverlay() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator color={"white"} size={"large"} />
+    </View>
+  );
 }
 
 export default LoadingOverlay;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
